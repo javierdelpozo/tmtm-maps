8c7c966bff6ee118a531563edee118e531e5ebad
Javier Del Pozo <contacto@javierdelpozo.com>
2018-12-21 13:56:08 +0100

    Data as function in test, constants out of test case, compact faster test code

--- tests/unit/MapBox.spec.js
+++ tests/unit/MapBox.spec.js
@@ -6,44 +6,32 @@ import Vuex from 'vuex';
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
+const store = getStore({}, {});
+const wrapper = shallowMount(MapBox, {
+  store,
+  localVue,
+  data() {
+    return {
+      baseUrl: 'https://api.tomtom.com/map/1/staticimage',
+      apiKey: 'k75KXr4my1Ttkfh44qW2ep3lGywgjgEG',
+      originY: null,
+      originX: null,
+      menuVisible: false,
+      loaded: false,
+    }
+  }
+})
+
 describe('MapBox.vue', () => {
   it('renders a map box container', () => {
-    const store = getStore({}, {});
-    const wrapper = shallowMount(MapBox, {
-      store,
-      localVue,
-      data: {
-        baseUrl: 'https://api.tomtom.com/map/1/staticimage',
-        apiKey: 'k75KXr4my1Ttkfh44qW2ep3lGywgjgEG',
-        originY: null,
-        originX: null,
-        menuVisible: false,
-        loaded: false,
-      }
-    })
     // Map Box
     expect(wrapper.contains('.mapbox')).to.equal(true);
 
     // Map container
     expect(wrapper.contains('.mapbox__container')).to.equal(true);
-  })
-});
+  });
 
-describe('MapBox.vue', () => {
   it('renders a map image', () => {
-    const store = getStore({}, {});
-    const wrapper = shallowMount(MapBox, {
-      store,
-      localVue,
-      data: {
-        baseUrl: 'https://api.tomtom.com/map/1/staticimage',
-        apiKey: 'k75KXr4my1Ttkfh44qW2ep3lGywgjgEG',
-        originY: null,
-        originX: null,
-        menuVisible: false,
-        loaded: false,
-      }
-    })
     // Map image
     expect(wrapper.contains('img')).to.equal(true);
   })
